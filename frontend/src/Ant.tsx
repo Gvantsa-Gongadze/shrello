03837a1701f0a88a8bfc818a76602db6211af24a
Gvantsa5 <ggong2016@agruni.edu.ge>
2021-03-03 14:08:21 +0400

    added ant design and created ant component

---
+++ frontend/src/Ant.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from 'antd';
+
+const Ant = () => {
+  return (
+    <div className="App">
+        <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default Ant;
