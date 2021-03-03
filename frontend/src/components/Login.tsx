b67f4fc6938256fb49cbe6782eca1ce3c877d334
Gvantsa5 <ggong2016@agruni.edu.ge>
2021-03-03 23:53:22 +0400

    created login page

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,68 @@
+import { Form, Input, Button, Checkbox, Card, Space } from 'antd';
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 12 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 4, span: 16 },
+};
+
+const Login = () => {
+    const onFinish = (values: any) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <Space {...layout}>
+            <Card 
+                title="Log in to Shrello" 
+                style={{ width: 500 }}
+            >
+                <Form
+                    {...layout}
+                    name='basic'
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item 
+                        {...tailLayout}
+                        name="remember" 
+                        valuePropName="checked"
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item {...tailLayout}>
+                        <Button type="primary" htmlType="submit">
+                            Log in
+                        </Button>
+                    </Form.Item>
+                    <a href="/registration">Sign up for an account</a>
+                </Form>
+            </Card>
+        </Space>
+    )
+}
+
+export default Login
