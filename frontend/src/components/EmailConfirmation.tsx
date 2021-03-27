48cc3eb2045aff4f888dc70fe64adc0f88a821ef
Gvantsa5 <ggong2016@agruni.edu.ge>
2021-03-27 18:09:06 +0400

    deleted useState from email confirmation page

--- frontend/src/components/EmailConfirmation.tsx
+++ frontend/src/components/EmailConfirmation.tsx
@@ -1,20 +1,17 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 function EmailConfirmation() {
     const { id } = useParams<{ id: string }>();
-    const [confirmed, setConfirmed] = useState(false)
 
     useEffect(() => {
         axios.put(`http://localhost:3000/users/${id}`, {confirmed: true})
         .then(res => {
-            console.log(confirmed);
-            setConfirmed(res.data.confirmed);
+            console.log(res.data.confirmed);
         })
         .catch((err) => {
             console.error(err);
-            setConfirmed(false);
         });
     });
 
